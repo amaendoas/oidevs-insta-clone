9d2cb016b2b3bf4a6db2f8b4de577c753bc09116
Amanda Guerra <guerraamanda1@hotmail.com>
2023-02-10 19:05:29 -0300

    tabs menu

--- src/components/TabItem/styles.js
+++ src/components/TabItem/styles.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
+import { Text } from "../../ui/Text";
 
 export const Container = styled.button`
   display: flex;
   align-items: center;
   padding-top: 1rem;
   gap: 5px;
-  
+
   ${({activeTab}) => activeTab ? "border-top: 2px solid #fff" : ""}
   
+`
+
+export const TabText = styled(Text)`
+  text-transform: uppercase;
 `
--- src/components/TabItem/tabItem.jsx
+++ src/components/TabItem/tabItem.jsx
@@ -1,13 +1,12 @@
-import { Text } from "../../ui/Text"
 import * as S from "./styles"
 
 export const TabItem = ({text, icon: Icon, size, iconSize, activeTab, onClick}) => {
   return (
     <S.Container onClick={onClick} activeTab={activeTab}>
       {Icon && <Icon size={iconSize || 20} color={activeTab ? '#fff' : '#858585'} />}
-      <Text size={size} color={activeTab ? '#fff' : '#858585'} >
+      <S.TabText size={size} color={activeTab ? '#fff' : '#858585'} >
         {text}
-      </Text>
+      </S.TabText>
     </S.Container>
   )
 }
